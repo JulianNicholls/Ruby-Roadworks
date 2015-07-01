a6dcf60f9e8702b895b9ac7ce2af8a8ea0860421
Julian Nicholls <juliannicholls29@gmail.com>
2015-07-01 09:19:56 +0100

    Updated to say 29 Jun data. Re-arranged holder.

--- load_roadworks.rb
+++ load_roadworks.rb
@@ -2,8 +2,8 @@
 
 require 'loader'
 
-xml_file = ARGV[0] || "ha_roadworks_2015_06_15.xml"
-loader = RoadworksLoaderFile.new xml_file, false
+xml_file = ARGV[0]
+loader = RoadworksLoaderFile.new xml_file
 count = loader.count
 if loader.count != 0      # Any records?
   print "There are #{count} records at present. Delete them? (Y/N) "
