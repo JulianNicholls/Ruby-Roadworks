d7c9657abfcf439652ca891d906215d691db381c
Julian Nicholls <juliannicholls29@gmail.com>
2015-08-18 19:34:22 +0100

    Objectified command line parsing in load_roadworks.

--- load_roadworks.rb
+++ load_roadworks.rb
@@ -3,50 +3,63 @@
 require 'optparse'
 require 'loader'
 
-options = {
-  remote:     false,
-  force:      false,
-  noforce:    false,
-  verbose:    false,
-  progress:   100
-}
 
-parser = OptionParser.new do |opts|
-  opts.banner = 'Usage:  load_roadworks.rb <filename> [options]'
+class CommandLineParser
+  def initialize
+    @options = {
+      remote:     false,
+      force:      false,
+      noforce:    false,
+      verbose:    false,
+      progress:   100
+    }
 
-  opts.on('-r', '--remote',
-          'Update the Heroku Database (Default: local).') do
-    options[:remote] = true
-    options[:progress] = 50
+    @parser = OptionParser.new do |opts|
+      opts.banner = "Usage:\n\tload_roadworks.rb <filename> [options]"
+      opts.separator ""
+
+      opts.on('-r', '--remote',
+              'Update the Heroku Database (Default: local).') do
+        @options[:remote] = true
+        @options[:progress] = 50
+      end
+
+      opts.on('-f', '--force',
+              'Force the update, deleting all previous data (Default: Ask).') do
+        @options[:force] = true
+      end
+
+      opts.on('-n', '--noforce',
+              'Skip the update and return failure if there is previous data.') do
+        @options[:noforce] = true
+      end
+
+      opts.on('-v', '--verbose', 'Turn on progress updates.') do
+        @options[:verbose] = true
+      end
+
+      opts.on_tail('-h', '--help', 'Show this help') do
+        puts opts
+        exit
+      end
+    end
   end
 
-  opts.on('-f', '--force',
-          'Force the update, deleting all previous data (Default: Ask).') do
-    options[:force] = true
-  end
+  def parse
+    @parser.parse!
 
-  opts.on('-n', '--noforce',
-          'Skip the update and return failure if there is previous data.') do
-    options[:noforce] = true
-  end
-
-  opts.on('-v', '--verbose',
-          'Turn on progress updates.') do
-    options[:verbose] = true
-  end
-
-  opts.on_tail('-h', '--help', 'Show this help') do
-    puts opts
-    exit
+    @options
+  rescue => err
+    puts "Argument Error: #{err.message}"
+    exit 1
   end
 end
 
-begin
-  parser.parse!
-rescue => e
-  puts 'Argument Error: ' + e.message
-  exit 1
-end
+#----------------------------------------------------------------------------
+# Main
+#----------------------------------------------------------------------------
+
+options = CommandLineParser.new.parse
 
 if ARGV.count != 1
   puts "\nArgument Error: XML Filename must be specified"
