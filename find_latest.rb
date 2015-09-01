e832a1a18f4195fa1de4b62c5e31e463ee985bf3
Julian Nicholls <juliannicholls29@gmail.com>
2015-09-01 18:43:08 +0100

    Re-factor logging.
    A logger is now injected as a dependency to the XML file collector class.
    The latest and destination filenames are lazily evaluated.

--- find_latest.rb
+++ find_latest.rb
@@ -3,6 +3,7 @@
 require 'open-uri'
 require 'nokogiri'
 require 'loader'
+require 'logger'
 
 # Find the files, which are in anchors in list elements in a dropdown menu.
 # There are three entries in each dropdown, only the correct one links to a
@@ -11,28 +12,24 @@ class Finder
   DATA_PAGE  = 'http://data.gov.uk/dataset/highways_agency_planned_roadworks'
   DATA_XPATH = '//div[@class="dropdown"]/ul/li/a[contains(@href,"http://")]'
 
-  def initialize(verbose)
-    @verbose = verbose
-    puts 'Searching...' if verbose
+  def initialize(logger)
+    @logger = logger
+    logger.puts 'Searching...'
 
     noko = Nokogiri::HTML open(DATA_PAGE)
     @files = noko.xpath DATA_XPATH
 
-    @latest   = sorted_files
-    @filename = File.split(@latest).last
-
-    puts "\nLatest File: #{@filename}" if verbose
+    logger.puts "\nLatest File: #{filename}"
   end
 
   def save_file
     return unless confirm_write
 
-    print 'Writing... ' if @verbose
+    @logger.print 'Writing... '
 
-    output = open @filename, 'w'
-    bytes  = output.write xml
+    bytes = File.write filename, xml
 
-    puts "#{bytes} Bytes." if @verbose
+    @logger.puts "#{bytes} Bytes."
   end
 
   def load_new_roadworks
@@ -45,26 +42,34 @@ class Finder
 
   # Recently there have been a couple of mistakes with naming on the site:
   #   First was a couple of files named ha-roadworks... instead of ha_roadworks
-  #   Early August '15 saw a file name he_roadworks...
+  #   Early August '15 saw a couple of file names he_roadworks...
   def sorted_files
     @files.map { |file| file['href'] }.sort_by do |href|
       href.gsub(/h.[_-]roadworks/, 'ha-roadworks')
-    end.reverse.first
+    end.reverse
   end
 
   def confirm_write
-    return true unless File.exist? @filename
+    return true unless File.exist? filename
 
-    print "\n#{@filename} exists, overwrite? (Y/N) "
+    print "\n#{filename} exists, overwrite? (Y/N) "
     answer = $stdin.gets.downcase
     answer[0] == 'y'
   end
 
   def xml
-    xml_file = open @latest
+    xml_file = open latest
     @xml ||= xml_file.read
+  end
+
+  def latest
+    @latest = sorted_files.first
+  end
+
+  def filename
+    @filename ||= File.split(latest).last
   end
 end
 
-finder = Finder.new(:verbose)
+finder = Finder.new(OutLogger)
 finder.save_file
