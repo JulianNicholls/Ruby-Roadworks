18b456379f4072e024046a7055ac62fceaa07c3c
Julian Nicholls <juliannicholls29@gmail.com>
2015-09-01 18:45:12 +0100

    Re-factor command line parser.
    Parser option building distributed.

--- load_roadworks.rb
+++ load_roadworks.rb
@@ -13,45 +13,65 @@ class CommandLineParser
       verbose:    false,
       progress:   100
     }
+  end
 
-    @parser = OptionParser.new do |opts|
+  def parser
+    OptionParser.new do |opts|
       opts.banner = "Usage:\n\tload_roadworks.rb <filename> [options]"
       opts.separator ''
 
-      opts.on('-r', '--remote',
-              'Update the Heroku Database (Default: local).') do
-        @options[:remote] = true
-        @options[:progress] = 50
-      end
-
-      opts.on('-f', '--force',
-              'Force the update, deleting all previous data (Default: Ask).') do
-        @options[:force] = true
-      end
-
-      opts.on('-n', '--noforce',
-              'Exit with a failure if there is previous data.') do
-        @options[:noforce] = true
-      end
-
-      opts.on('-v', '--verbose', 'Turn on progress updates.') do
-        @options[:verbose] = true
-      end
-
-      opts.on_tail('-h', '--help', 'Show this help') do
-        puts opts
-        exit
-      end
+      add_remote opts
+      add_force opts
+      add_noforce opts
+      add_verbose opts
+      add_help opts
     end
   end
 
   def parse
-    @parser.parse!
+    parser.parse!
 
     @options
   rescue => err
     puts "Argument Error: #{err.message}"
     exit 1
+  end
+
+  private
+
+  def add_remote(opts)
+    opts.on('-r', '--remote',
+            'Update the Heroku Database (Default: local).') do
+      @options[:remote] = true
+      @options[:progress] = 50
+    end
+  end
+
+  def add_force(opts)
+    opts.on('-f', '--force',
+            'Force the update, deleting all previous data (Default: Ask).') do
+      @options[:force] = true
+    end
+  end
+
+  def add_noforce(opts)
+    opts.on('-n', '--noforce',
+            'Exit with a failure if there is previous data.') do
+      @options[:noforce] = true
+    end
+  end
+
+  def add_verbose(opts)
+    opts.on('-v', '--verbose', 'Turn on progress updates.') do
+      @options[:verbose] = true
+    end
+  end
+
+  def add_help(opts)
+    opts.on_tail('-h', '--help', 'Show this help') do
+      puts opts
+      exit
+    end
   end
 end
 
