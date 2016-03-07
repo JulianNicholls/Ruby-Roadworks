4987900961cc8174810175a90a633a312808aa36
Julian Nicholls <juliannicholls29@gmail.com>
2016-03-07 19:37:24 +0000

    Re-factor ArgParser

--- arg_parser.rb
+++ arg_parser.rb
@@ -25,64 +25,65 @@ class CommandLineParser
 
   def parser
     OptionParser.new do |opts|
-      start_help_text opts
-      add_options opts
+      @opts = opts
+      start_help_text
+      add_options
     end
   end
 
-  def start_help_text(opts)
-    opts.banner = "Usage:\n\tload_roadworks.rb <filename> [options]"
-    opts.separator ''
+  def start_help_text
+    @opts.banner = "Usage:\n\tload_roadworks.rb <filename> [options]"
+    @opts.separator ''
   end
 
-  def add_options(opts)
-    add_remote opts
-    add_force opts
-    add_noforce opts
-    add_verbose opts
-    add_progress opts
-    add_help opts
+  def add_options
+    add_remote
+    add_force
+    add_noforce
+    add_verbose
+    add_progress
+    add_help
   end
 
-  def add_remote(opts)
-    opts.on('-r', '--remote',
+  def add_remote
+    @opts.on('-r', '--remote',
             'Update the Heroku Database (Default: local).') do
       @options[:remote] = true
       @options[:progress] = 20 if @options[:progress] > 20
     end
   end
 
-  def add_force(opts)
-    opts.on('-f', '--force',
+  def add_force
+    @opts.on('-f', '--force',
             'Force the update, deleting all previous data (Default: Ask).') do
       @options[:force] = true
     end
   end
 
-  def add_noforce(opts)
-    opts.on('-n', '--noforce',
+  def add_noforce
+    @opts.on('-n', '--noforce',
             'Exit with a failure if there is previous data.') do
       @options[:noforce] = true
     end
   end
 
-  def add_verbose(opts)
-    opts.on('-v', '--verbose', 'Turn on progress updates.') do
+  def add_verbose
+    @opts.on('-v', '--verbose', 'Turn on progress updates.') do
       @options[:verbose] = true
     end
   end
 
-  def add_progress(opts)
-    opts.on('-p', '--progress VALUE',
+  def add_progress
+    @opts.on('-p', '--progress VALUE',
             'Set the number of records between progress updates.') do |value|
       @options[:verbose] = true
       @options[:progress] = value.to_i
     end
   end
 
-  def add_help(opts)
-    opts.on_tail('-h', '--help', 'Show this help') do
-      puts opts
+  def add_help
+    @opts.on_tail('-h', '--help', 'Show this help') do
+      puts @opts
       exit
     end
   end
